5276f4dcd8711ff1f4971bd104ff2cc7e0d12a54
RandalphvanVelden <45844254+RandalphvanVelden@users.noreply.github.com>
2022-03-04 13:34:55 +0100

    got the server side for the token

--- app/Http/Controllers/Authcontroller.php
+++ app/Http/Controllers/Authcontroller.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class Authcontroller extends Controller
+class AuthController extends Controller
 {
    public function login(Request $request)
    {
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/login', 'AuthController@login');
-Route::post('/register', 'AuthController@register');
-Route::middleware('auth:api')->post('/logout', 'AuthController@logout');
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
+Route::post('/register', 'App\Http\Controllers\AuthController@register');
+Route::middleware('auth:api')->post('/logout', 'App\Http\Controllers\AuthController@logout');
